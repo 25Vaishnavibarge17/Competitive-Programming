28f9ebef96706088af084e89a82085c79b231e24
Vaishnavi Barge <89184566+25Vaishnavibarge17@users.noreply.github.com>
2022-09-10 14:02:54 +0530

    Create Guess the Bottom Face.cpp

---
+++ Codechef Solutions/Guess the Bottom Face.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--){
+   int X;
+   cin>>X;
+   int res;
+   res=7-X;
+   cout<<res<<endl;
+   }
+   return 0;
+}
